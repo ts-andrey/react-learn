9364928f27a4e2983f60ced3d5cff6f44e4b1a5b
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2022-11-15 16:49:50 +0300

    feat: add list rendering logic

--- expense-tracker/src/components/expenseItem/ExpenseItem.js
+++ expense-tracker/src/components/expenseItem/ExpenseItem.js
@@ -4,10 +4,10 @@ import './ExpenseItem.css';
 function ExpenseItem(props) {
   return (
     <li className='expense-item'>
-      <ExpenseDate date={props.date} />
+      <ExpenseDate date={props.data.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>{props.amount}<span className='expense-item__price_currency'>$</span></div>
+        <h2>{props.data.title}</h2>
+        <div className='expense-item__price'>{props.data.amount}<span className='expense-item__price_currency'>$</span></div>
       </div>
     </li>
   );
--- expense-tracker/src/components/expenseList/ExpenseList.js
+++ expense-tracker/src/components/expenseList/ExpenseList.js
@@ -4,10 +4,9 @@ import ExpenseItem from '../expenseItem/ExpenseItem';
 function ExpenseList(props) {
   return (
     <ul className='expense-list'>
-      <ExpenseItem date={props.expenses[0].date} title={props.expenses[0].title} amount={props.expenses[0].amount} />
-      <ExpenseItem date={props.expenses[1].date} title={props.expenses[1].title} amount={props.expenses[1].amount} />
-      <ExpenseItem date={props.expenses[2].date} title={props.expenses[2].title} amount={props.expenses[2].amount} />
-      <ExpenseItem date={props.expenses[3].date} title={props.expenses[3].title} amount={props.expenses[3].amount} />
+      {props.expenses.map((expense) => {
+        return <ExpenseItem data={expense} />
+      })}
     </ul>
   )
 }
