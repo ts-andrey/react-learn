c4c26297fdb14ba5318c02f44242995877d0f156
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2022-12-02 14:29:14 +0300

    feat: prepare basic setup

--- effect-reducer-context/src/App.js
+++ effect-reducer-context/src/App.js
@@ -1,9 +1,27 @@
-import {Fragment} from 'react';
+import { Fragment, useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <Fragment>
-      
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
     </Fragment>
   );
 }
