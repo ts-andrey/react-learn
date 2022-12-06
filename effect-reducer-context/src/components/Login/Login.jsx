19d089af1cb28aeaaef2a3cf79d9331d426d96b9
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2022-12-06 21:30:11 +0300

    feat: add forward ref

--- effect-reducer-context/src/components/Login/Login.jsx
+++ effect-reducer-context/src/components/Login/Login.jsx
@@ -5,6 +5,7 @@ import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 import AuthContext from '../../store/auth-context';
 import Input from '../UI/Input/Input';
+import { useRef } from 'react';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -27,10 +28,6 @@ const passwordReducer = (state, action) => {
 }
 
 const Login = (props) => {
-  // const [enteredEmail, setEnteredEmail] = useState('');
-  // const [emailIsValid, setEmailIsValid] = useState();
-  // const [enteredPassword, setEnteredPassword] = useState('');
-  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, { value: '', isValid: true });
@@ -40,6 +37,9 @@ const Login = (props) => {
   const { isValid: passwordIsValid } = passwordState;
 
   const authContext = useContext(AuthContext);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -55,18 +55,10 @@ const Login = (props) => {
 
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: 'USER_INPUT', value: event.target.value })
-
-    // setFormIsValid(
-    //   emailState.isValid && passwordState.isValid
-    // );
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({ type: 'USER_INPUT', value: event.target.value })
-
-    // setFormIsValid(
-    //   emailState.isValid && passwordState.isValid
-    // );
   };
 
   const validateEmailHandler = () => {
@@ -79,13 +71,20 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authContext.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      authContext.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           id='email'
           label='E-Mail'
           type='email'
@@ -95,6 +94,7 @@ const Login = (props) => {
           onBlur={validateEmailHandler}
         />
         <Input
+          ref={passwordInputRef}
           id='password'
           label='Password'
           type='password'
@@ -104,7 +104,10 @@ const Login = (props) => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button
+            type="submit"
+            className={classes.btn}
+          >
             Login
           </Button>
         </div>
--- effect-reducer-context/src/components/UI/Input/Input.jsx
+++ effect-reducer-context/src/components/UI/Input/Input.jsx
@@ -1,6 +1,19 @@
+import { useRef, useImperativeHandle, forwardRef } from 'react';
 import classes from './Input.module.css';
 
-const Input = props => {
+const Input = forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus: activate
+    }
+  });
+
   return (
     <div
       className={`${classes.control} ${props.isValid === false ? classes.invalid : ''
@@ -8,6 +21,7 @@ const Input = props => {
     >
       <label htmlFor="email">{props.label}</label>
       <input
+        ref={inputRef}
         type={props.type}
         id={props.id}
         value={props.value}
@@ -16,6 +30,6 @@ const Input = props => {
       />
     </div>
   )
-}
+})
 
 export default Input;
