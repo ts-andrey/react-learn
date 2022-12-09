19a93161076eb7622e27691a7d8eede2495f6cff
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2022-12-09 17:15:02 +0300

    feat: add cart and modal

--- food-order-app/src/App.jsx
+++ food-order-app/src/App.jsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import { Fragment } from 'react';
+import Cart from './components/Cart/Cart';
 
 import Header from './components/Layout/Header/Header';
 import Meals from './components/Meals/Meals';
 
 function App() {
+  const [cartIsVisible, setCartIsVisible] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsVisible(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsVisible(false);
+  }
+
   return (
     <Fragment>
-      <Header />
+      {cartIsVisible && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
