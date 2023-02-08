494ea5be004b7d682ab0bf73922021944f1a16e3
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2023-02-08 13:55:46 +0300

    feat: add Modal component

--- post-app/src/components/PostList.jsx
+++ post-app/src/components/PostList.jsx
@@ -2,11 +2,29 @@ import classes from './PostList.module.css';
 
 import Post from './Post';
 import NewPost from './NewPost';
+import Modal from './Modal';
+import { useState } from 'react';
 
 function PostList() {
-  return (
+  const [isModalVisible, setIsModalVisible] = useState(true);
+
+  function hideModalHandler() {
+    setIsModalVisible(false);
+  }
+
+
+  let modalContent;
+  if (isModalVisible) {
+    modalContent = (
+      <Modal onClose={hideModalHandler}>
+        <NewPost />
+      </Modal>
+    )
+  }
+
+  return ( 
     <>
-      <NewPost />
+      {modalContent}
       <ul className={classes['post-list']}>
         <Post author={'Author_1'} body={'SomeText_1'} />
         <Post author={'Author_2'} body={'SomeText_2'} />
